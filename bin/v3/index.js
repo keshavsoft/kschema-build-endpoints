3c21fbd510cce75eb6bd4e841d983072b32c7f3d
keshavsoft <nknnkumar@yahoo.com>
2026-07-27 20:50:35 +0530

    v3

---
+++ bin/v3/index.js
@@ -0,0 +1,39 @@
+import pullEndpoints from "kschema-pull-endpoints";
+import pullMethods from "kschema-pull-methods";
+import fsReadContent from "node-fs-read-content";
+
+const startFunc = ({ toPath, inAction = "Crud", inTargetPath }) => {
+    switch (inAction) {
+        case "Crud":
+            let endPointsArray = [];
+
+            let endpointsWithContent = [];
+
+            const fromPullEndPoints = pullEndpoints({ toPath, inTargetPath });
+
+            endpointsWithContent = fsReadContent({ filePaths: fromPullEndPoints });
+
+            endpointsWithContent.forEach(loopEndPoint => {
+                const methodsContent = pullMethods({
+                    filePath: loopEndPoint.fileFullPath,
+                    fileContent: loopEndPoint.fileContent
+                });
+                // console.log("aaaaaaaa : ", element);
+
+                methodsContent.forEach(loopMethodsContent => {
+                    endPointsArray.push({
+                        endPointContent: loopEndPoint,
+                        methodsContent: loopMethodsContent
+                    });
+                });
+            });
+
+            return endPointsArray;
+            break;
+        default:
+            break;
+    }
+    return true;
+};
+
+export default startFunc;
