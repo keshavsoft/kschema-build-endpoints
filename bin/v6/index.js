bffebe6dc1c57cc1d485254b8b557eea9127d985
keshavsoft <nknnkumar@yahoo.com>
2026-08-04 16:04:11 +0530

    to test

--- bin/v6/index.js
+++ bin/v6/index.js
@@ -1,12 +1,9 @@
-import path from "path";
-
 import pullEndpoints from "kschema-pull-endpoints";
 import pullMethods from "kschema-pull-methods";
 import fsReadContent from "node-fs-read-content";
-import anyJsStory from "pattern-collector-anyjs-story";
+// import anyJsStory from "pattern-collector-anyjs-story";
 
-const startFunc = ({ toPath, inAction = "Crud",
-    inTargetPath, extractRegex }) => {
+const startFunc = ({ toPath, inAction = "Crud", inTargetPath }) => {
 
     switch (inAction) {
         case "Crud":
@@ -18,30 +15,31 @@ const startFunc = ({ toPath, inAction = "Crud",
 
             endpointsWithContent = fsReadContent({ filePaths: fromPullEndPoints });
 
-            endpointsWithContent.forEach(element => {
-                const fileContent = element?.fileContent;
-                const fileType = element?.fileType;
+            // endpointsWithContent.forEach(element => {
+            //     const fileContent = element?.fileContent;
+            //     const fileType = element?.fileType;
 
-                const story = anyJsStory({
-                    fileContent, fileType
-                });
-            });
-
-            // endpointsWithContent.forEach(loopEndPoint => {
-            //     const methodsContent = pullMethods({
-            //         filePath: loopEndPoint.fileFullPath,
-            //         fileContent: loopEndPoint.fileContent,
-            //         inTargetPath, extractRegex
-            //     });
-            //     // console.log("aaaaaaaa : ", element);
-
-            //     methodsContent.forEach(loopMethodsContent => {
-            //         endPointsArray.push({
-            //             endPointContent: loopEndPoint,
-            //             methodsContent: loopMethodsContent
-            //         });
+            //     const story = anyJsStory({
+            //         fileContent, fileType
             //     });
             // });
+
+            endpointsWithContent.forEach(loopEndPoint => {
+                const methodsContent = pullMethods({
+                    fileType: "fromEndPointsJs",
+                    filePath: loopEndPoint.fileFullPath,
+                    fileContent: loopEndPoint.fileContent,
+                    inTargetPath
+                });
+                // console.log("aaaaaaaa : ", element);
+
+                methodsContent.forEach(loopMethodsContent => {
+                    endPointsArray.push({
+                        endPointContent: loopEndPoint,
+                        methodsContent: loopMethodsContent
+                    });
+                });
+            });
 
             return endPointsArray;
             break;
