c36526c28ccbda08674dbce18ea83516de62629d
KeshavSoft <nknnkumar@yahoo.com>
2026-08-05 15:14:01 +0530

    del

--- test/v2/api/v1/doctors/modify/saveData.js
+++
@@ -1,9 +0,0 @@
-import { JSONFilePreset } from 'lowdb/node';
-
-const saveData = async ({ inTablePath, inData }) => {
-    const db = await JSONFilePreset(inTablePath, []);
-    db.data = inData;
-    await db.write();
-};
-
-export default saveData;
